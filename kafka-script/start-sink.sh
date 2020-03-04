1f324664d993f22be83bc626fa646afb22c8e8b7
gwangsoo <gwangsoo72@gmail.com>
2020-03-04 17:06:38 +0900

    kafka connect script 추가

---
+++ kafka-script/start-sink.sh
@@ -0,0 +1,1 @@
+/opt/kafka/bin/connect-standalone.sh -deamon connect-standalone.properties mysql-sink.properties
---
+++ kafka-script/start-source.sh
@@ -0,0 +1,1 @@
+/opt/kafka/bin/connect-standalone.sh -daemon connect-standalone.properties mysql-source.properties
